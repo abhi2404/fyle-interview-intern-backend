347fd89872649d6c6a7f24e4f48fbe88bac6a906
abhi2417 <abhishek.1923cs1175@kiet.edu>
2023-04-02 16:43:44 +0530

    added missing apis -> tested the api's using postman

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -63,6 +63,7 @@ class Assignment(db.Model):
     def submit(cls, _id, teacher_id, principal: Principal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.state ==AssignmentStateEnum.DRAFT,'only a draft assignment can be submitted')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
 
@@ -75,3 +76,24 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    ## method to get list of assignments##
+    @classmethod
+    def get_assignments_submitted(cls, teacher_id):
+        return cls.filter(cls.teacher_id==teacher_id, cls.state=='SUBMITTED').all()
+
+    ## method to set grade for and assignment##
+    @classmethod
+    def set_grade(cls, _id, _grade, principal: Principal ):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'Assignment not found with these data')
+        assertions.assert_valid(assignment.teacher_id==principal.teacher_id,
+                                'assignment was not submitted to respective teacher')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                                'Submitted assignment can only be graded')
+        assignment.grade = _grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -25,6 +25,18 @@ def test_get_assignments_teacher_2(client, h_teacher_2):
         assert assignment['teacher_id'] == 2
         assert assignment['state'] == 'SUBMITTED'
 
+def test_get_assignments_teacher_3(client, h_teacher_3):
+    response = client.get(
+        '/teacher/assignments',
+        headers=h_teacher_3
+    )
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    for assignment in data:
+        assert assignment['teacher_id'] == 3
+        assert assignment['state'] == 'SUBMITTED'
 
 def test_grade_assignment_cross(client, h_teacher_2):
     """
